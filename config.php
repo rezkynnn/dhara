e5bfb39c0a4f51b9f9cb5ed7325337ec8ce5a85e
rezkyn <35022812+rezkynnn@users.noreply.github.com>
2018-12-29 13:32:54 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+///Jangan hapus tanpa kutip nya(")
+///Jangan pernah sekali-sekali jika gak mau error pas php bot.php
+///Ganti xxxxxxx dengan punya kalian
+///Capture dulu apk nya ambil yang di butuhkan
+$ticket = "sHp2r312otmCpnGns3mCqJhovNiBh3jOu9CcorPbg8q9o4prip-nz4O2fWGzeH2qho28y393ns6v4H6htLV3za2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ";
+$meid = "efad716b478c2e7d";
+$os= "android";
+$sign= "27d8cca687d1900b07cc424381182505d6cab451cbd08eae1f8a6f048094a0cd";
